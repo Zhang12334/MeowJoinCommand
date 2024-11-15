b98a85b7d225fbb75f5a271ea3b7d50ef4835748
Zhang12334 <3143448821@qq.com>
2024-11-15 23:38:15 +0800

    reload language-fix

--- src/main/java/com/meow/meowjoincommand/MeowJoinCommand.java
+++ src/main/java/com/meow/meowjoincommand/MeowJoinCommand.java
@@ -9,18 +9,20 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.List;
-import java.util.ArrayList;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.configuration.file.FileConfiguration;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.lang.reflect.Field;
+import java.util.List;
+import java.util.ArrayList;
 
 public class MeowJoinCommand extends JavaPlugin implements Listener {
 
     private ConfigHandler configHandler;
-    // 一堆存储消息的变量
+
+    // 存储语言消息的变量
     private String startupMessage;
     private String shutdownMessage;
     private String notenableMessage;
@@ -41,9 +43,10 @@ public class MeowJoinCommand extends JavaPlugin implements Listener {
     private String unknowncommandMessage;
     private String commandnotcorrectMessage;
 
+    // 获取语言变量的方法
     public String getLanguage(String variableName) {
         try {
-            // 使用反射根据变量名获取对应的字段
+            // 使用反射根据变量名获取字段
             Field field = this.getClass().getDeclaredField(variableName);
             field.setAccessible(true);  // 使私有字段可以访问
             return (String) field.get(this);  // 返回该字段的值
@@ -52,14 +55,14 @@ public class MeowJoinCommand extends JavaPlugin implements Listener {
             return null;  // 如果出现错误，返回null
         }
     }
-    
+
     @Override
     public void onEnable() {
-        //bstats
         int pluginId = 23901;
         Metrics metrics = new Metrics(this, pluginId);
-        loadLanguage(); // 加载语言配置
-        // 加载配置文件
+        
+        // 加载配置和语言设置
+        loadLanguage();
         saveDefaultConfig();
         configHandler = new ConfigHandler(this);
 
@@ -69,10 +72,12 @@ public class MeowJoinCommand extends JavaPlugin implements Listener {
         } else {
             getLogger().warning(notenableMessage);
         }
+        
         String currentVersion = getDescription().getVersion();
         getLogger().info(nowusingversionMessage + " v" + currentVersion);
         getLogger().info(checkingupdateMessage);
-        // 异步更新检查
+        
+        // 异步检查更新
         new BukkitRunnable() {
             @Override
             public void run() {
@@ -81,6 +86,7 @@ public class MeowJoinCommand extends JavaPlugin implements Listener {
         }.runTaskAsynchronously(this);
     }
 
+    // 加载语言设置
     private void loadLanguage() {
         FileConfiguration config = getConfig();
         String language = config.getString("language", "zh_cn");
@@ -133,54 +139,44 @@ public class MeowJoinCommand extends JavaPlugin implements Listener {
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
         List<String> suggestions = new ArrayList<>();
-        if (args.length == 1) {         
+        if (args.length == 1) {
             suggestions.add("reload");
         }
         return suggestions;
     }
 
     private void check_update() {
-        // 获取当前版本号
         String currentVersion = getDescription().getVersion();
-        // github加速地址，挨个尝试
         String[] githubUrls = {
             "https://ghp.ci/",
             "https://raw.fastgit.org/",
             ""
-            //最后使用源地址
         };
-        // 获取 github release 最新版本号作为最新版本
-        // 仓库地址：https://github.com/Zhang12334/MeowJoinCommand
         String latestVersionUrl = "https://github.com/Zhang12334/MeowJoinCommand/releases/latest";
-        // 获取版本号
+
         try {
             String latestVersion = null;
             for (String url : githubUrls) {
                 HttpURLConnection connection = (HttpURLConnection) new URL(url + latestVersionUrl).openConnection();
                 connection.setInstanceFollowRedirects(false); // 不自动跟随重定向
                 int responseCode = connection.getResponseCode();
-                if (responseCode == 302) { // 如果 302 了
+                if (responseCode == 302) { // 如果是 302 重定向
                     String redirectUrl = connection.getHeaderField("Location");
                     if (redirectUrl != null && redirectUrl.contains("tag/")) {
-                        // 从重定向URL中提取版本号
                         latestVersion = extractVersionFromUrl(redirectUrl);
-                        break; // 找到版本号后退出循环
+                        break;
                     }
                 }
                 connection.disconnect();
-                if (latestVersion != null) {
-                    break; // 找到版本号后退出循环
-                }
             }
+
             if (latestVersion == null) {
                 getLogger().warning(checkfailedMessage);
                 return;
             }
-            // 比较版本号
+
             if (isVersionGreater(latestVersion, currentVersion)) {
-                // 如果有新版本，则提示新版本
                 getLogger().warning(updateavailableMessage + " v" + latestVersion);
-                // 提示下载地址（latest release地址）
                 getLogger().warning(updateurlMessage + " https://github.com/Zhang12334/MeowJoinCommand/releases/latest");
                 getLogger().warning(oldversionmaycauseproblemMessage);
             } else {
@@ -191,24 +187,19 @@ public class MeowJoinCommand extends JavaPlugin implements Listener {
         }
     }
 
-    // 版本比较
     private boolean isVersionGreater(String version1, String version2) {
         String[] v1Parts = version1.split("\\.");
         String[] v2Parts = version2.split("\\.");
         for (int i = 0; i < Math.max(v1Parts.length, v2Parts.length); i++) {
             int v1Part = i < v1Parts.length ? Integer.parseInt(v1Parts[i]) : 0;
             int v2Part = i < v2Parts.length ? Integer.parseInt(v2Parts[i]) : 0;
-            if (v1Part > v2Part) {
-                return true;
-            } else if (v1Part < v2Part) {
-                return false;
-            }
+            if (v1Part > v2Part) return true;
+            else if (v1Part < v2Part) return false;
         }
         return false;
     }
-    
+
     private String extractVersionFromUrl(String url) {
-        // 解析 302 URL 中的版本号
         int tagIndex = url.indexOf("tag/");
         if (tagIndex != -1) {
             int endIndex = url.indexOf('/', tagIndex + 4);
@@ -235,7 +226,6 @@ public class MeowJoinCommand extends JavaPlugin implements Listener {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (cmd.getName().equalsIgnoreCase("meowjoincommand")) {
             if (args.length == 1 && args[0].equalsIgnoreCase("reload")) {
-                // 检查是否是具有权限的用户
                 if (sender.hasPermission("meowjoincommand.reload")) {
                     reloadConfig();
                     loadLanguage(); // 加载语言配置
