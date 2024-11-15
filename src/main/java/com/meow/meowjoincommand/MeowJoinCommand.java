4c454ed354923d59eeec42f96126b973f8b79bf5
Zhang12334 <3143448821@qq.com>
2024-11-15 22:03:37 +0800

    add update check

--- src/main/java/com/meow/meowjoincommand/MeowJoinCommand.java
+++ src/main/java/com/meow/meowjoincommand/MeowJoinCommand.java
@@ -9,6 +9,11 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+import java.util.ArrayList;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class MeowJoinCommand extends JavaPlugin implements Listener {
 
